f08e07c5973d02926ce9b98b0de20fd11ca52ac8
Leta Montopoli <lmontopo@gmail.com>
2014-10-20 12:55:03 -0400

    Added our first maze solver for the maze that we wrote on Friday. It get stuck turning corers, but its kind of working.

--- maze.py
+++ maze.py
@@ -1,31 +1,40 @@
 import pygame
 
-#Define some colours:
+# Define some colours:
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
+# Define some variables
 clock = pygame.time.Clock()
-
 done = False
 
+WIDTH = 500
+HEIGHT = 700
+block_size = 20
+
+# Initiate screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# An array containing all the black square indices
+entire_maze = []
 
 def generate_border(width, height):
 	list = []
 	y = 0
 	x = 0
 	while y < height:
-		list.append((0,y+20)) #west
-		list.append((width-20,y)) #east
-		y +=20
+		list.append((0, y + block_size)) #west
+		list.append((width - block_size,y)) #east
+		y += block_size
 	list.pop() #to make an exit
 	
 	while x < width:
-		list.append((x+40,0)) #North
-		list.append((x,height-20)) #south
+		list.append((x + 2 * block_size, 0)) #North
+		list.append((x, height - block_size)) #south
 		x += 20
 	list.pop() #to make an exit
-	list.remove((width-20, height - 40)) #to make an exit
+	list.remove((width - block_size, height - 2*block_size)) #to make an exit
 	return list
 	
 #generates a single wall within the maze	
@@ -33,13 +42,11 @@ def generate_wall(width, height, x, y):
 	list = []
 	while y < height:
 		list.append((x,y))
-		y += 20
+		y += block_size
 	return list
-		
-	
 
 def draw_square(x,y):
-	pygame.draw.rect(screen, BLACK, [x,y,20,20],0)
+	pygame.draw.rect(screen, BLACK, [x, y, block_size, block_size], 0)
 
 #takes a list of indices to draw squares, then draws them
 def draw_maze(list):
@@ -51,15 +58,66 @@ def generate_maze(width, height):
 	list = []
 	x = 20
 	while x < (width - 20):
-		list.extend(generate_wall(500, 680, x, 40))
-		list.extend(generate_wall(500, 660, x + 40, 20))
+		list.extend(generate_wall(WIDTH, HEIGHT - block_size, x, 40))
+		list.extend(generate_wall(WIDTH, HEIGHT - 2 * block_size, x + 40, 20))
 		x +=80
 	return list
 
-#initiate screen
-screen = pygame.display.set_mode((500,700))
+# A class of maze solvers
+class Solver:
+	def __init__(self, start_pos, end_pos, color):
+		self.cur_pos = start_pos
+		self.end_pos = end_pos
+		self.color = color
 
-# --- Main Program Loop
+	def display(self):
+		pygame.draw.rect(screen, self.color, [self.cur_pos[0], self.cur_pos[1], 20, 20], 0)
+
+	def check_surroundings(self, maze):
+		# check if we can move down
+		down = [self.cur_pos[0], self.cur_pos[1] + block_size]
+		if (not (down[0], down[1]) in maze) and down[1] < HEIGHT:
+			return "DOWN"
+
+		# check if we can move up
+		up = [self.cur_pos[0], self.cur_pos[1] - block_size]
+		if (not (up[0], up[1]) in maze) and up[1] > 0:
+			return "UP"
+
+		# check if we can move right
+		right = [self.cur_pos[0] + block_size, self.cur_pos[1]]
+		if (not (right[0], right[1]) in maze) and right[0] < WIDTH:
+			return "RIGHT"
+
+		# check if we can move right
+		left = [self.cur_pos[0] - block_size, self.cur_pos[1]]
+		if (not (left[0], left[1]) in maze) and left[0] > 0:
+			return "LEFT"
+
+
+	# direction is a string UP DOWN LEFT RIGHT
+	def update(self, maze):
+		# Get the direction to move to
+		direction = self.check_surroundings(maze)
+
+		# Move
+		if direction == "RIGHT":
+			self.cur_pos[0] += block_size
+		elif direction == "LEFT":
+			self.cur_pos[0] -= block_size
+		elif direction == "UP":
+			self.cur_pos[1] -= block_size
+		elif direction == "DOWN":
+			self.cur_pos[1] += block_size
+
+#--------------- Construct the entire maze
+entire_maze.extend(generate_border(WIDTH, HEIGHT))
+entire_maze.extend(generate_maze(WIDTH, HEIGHT))
+
+# Create maze solver object
+solver = Solver([0, 0], (WIDTH, HEIGHT), GREEN)
+
+# --------------------------------- Main Program Loop ---------------------------- #
 while not done:
 	#--- Main event loop
 	for event in pygame.event.get():
@@ -68,12 +126,12 @@ while not done:
 	
 	# -- Game logic should go here
 	
-	
 	# -- First, clear the screen to white.
 	screen.fill(WHITE)
 	# -- Drawing should go here
-	draw_maze(generate_border(500,700)) #draw outer border
-	draw_maze(generate_maze(500,700)) #draw maze
+	draw_maze(entire_maze)
+	solver.display()
+	solver.update(entire_maze)
 	
 	# -- update the screen with whatever we've drawn.
 	pygame.display.flip()
